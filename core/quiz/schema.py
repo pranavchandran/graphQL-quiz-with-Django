bec0e8682f5ee68526a6accfebf850fd6f529094
pranav <pranav.chandran@gmail.com>
2021-04-15 13:13:19 +0530

    CRUD Completed

--- core/quiz/schema.py
+++ core/quiz/schema.py
@@ -45,4 +45,49 @@ class Query(graphene.ObjectType):
     def resolve_all_answers(root, info, id):
         return Answer.objects.filter(question=id)
 
-schema = graphene.Schema(query=Query)
+class CategoryMutation(graphene.Mutation):
+    class Arguments:
+        name = graphene.String(required=True)
+    category = graphene.Field(CategoryType)
+
+    @classmethod
+    def mutate(cls, root, info, name):
+        category = Category(name=name)
+        category.save()
+        return CategoryMutation(category=category)
+
+class QuizzesMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        id = graphene.Int(required=True)
+        delete_id = graphene.Int()
+
+    quiz = graphene.Field(QuizzesType)
+
+    @classmethod
+    def mutate(cls, root, info, title, id, delete_id=0):
+        if delete_id != 0:
+            quiz = Quizzes.objects.get(id=delete_id)
+            import pdb ; pdb.set_trace()
+            quiz.delete()
+            print(quiz)
+            if quiz.id is None:
+                return
+            import pdb ; pdb.set_trace()
+            
+            # return f'Deleted {delete_id}'
+        else:
+            quiz = Quizzes(title=title)
+            quiz.category_id = id
+            # category = Category(name=name)
+            quiz.save()
+            return QuizzesMutation(quiz=quiz)
+#fields = ("id", "title", "category", "quiz") 
+
+
+
+class Mutation(graphene.ObjectType):
+    update_category = CategoryMutation.Field()
+    update_quizzes = QuizzesMutation.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
